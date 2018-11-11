11740f2838e4904a8b0cc953b9a49fd6a239badb
Adam Dunsmoor <adamdunsmoor@pm.me>
2018-11-10 22:53:51 -0600

    progress on bluetooth with plugin

--- INSTALL.md
+++ INSTALL.md
@@ -5,13 +5,23 @@
 - `npm install -g nativescript --unsafe-perm`
 - `tns extension install nativescript-cloud`
 
+## Nativescript bluetooth
+- Github: https://github.com/eddyverbruggen/nativescript-bluetooth
+- `tns plugin add nativescript-bluetooth`
+    - `npm i nativescript-bluetooth --save-dev`
+
 ## App creation
 - `tns create dog-alert --template tns-template-blank-ng`
 
 ### Android phone
-- Enable US Debugging
+- Enable USB Debugging
     - Tutorial: https://developer.android.com/studio/debug/dev-options 
-
+- Download bluetooth log file
+    - Tutorial: http://www.fte.com/WebHelp/BPA600/Content/Documentation/WhitePapers/BPA600/Encryption/GettingAndroidLinkKey/RetrievingHCIlog.htm
+    - `adb pull /sdcard/btsnoop_hci.log bluetooth_logs`
+    - open file with wireshark
+        - `apt-get install wireshark`
+    - Shutterbug uses protocol: `L2CAP`
 # Commands
 
 ## Telerik Nativescript (tns)
@@ -19,7 +29,10 @@
 - `tns cloud build`
 - `tns run`   // run the app
 - `tns doctor`    // check for any issues
+- `tns build android`   // build app for Android
 
 ## Github
 - git config user.email "adamdunsmoor@pm.me"  // set email address
+- git config user.username "xinthose"  // set user name
 - git config user.email  // check email address
+- git config user.username  // check user name
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,121 @@
 import { Component, OnInit } from "@angular/core";
 
+import { connectionType, getConnectionType, startMonitoring, stopMonitoring } from "tns-core-modules/connectivity";
+
+import * as connectivity from "tns-core-modules/connectivity";
+
+import bluetooth = require('nativescript-bluetooth');
+
 @Component({
     selector: "Home",
     moduleId: module.id,
     templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
+    connectionType: string;
 
     constructor() {
-        // Use the component constructor to inject providers.
+        connectivity.startMonitoring((newConnectionType: number) => {
+            switch (newConnectionType) {
+                case connectivity.connectionType.none:
+                    this.connectionType = "None";
+                    console.log("Connection type changed to none.");
+                    break;
+                case connectivity.connectionType.wifi:
+                    this.connectionType = "Wi-Fi";
+                    console.log("Connection type changed to WiFi.");
+                    break;
+                case connectivity.connectionType.mobile:
+                    this.connectionType = "Mobile";
+                    console.log("Connection type changed to mobile.");
+                    break;
+                case connectivity.connectionType.ethernet:
+                    this.connectionType = "Ethernet";
+                    console.log("Connection type changed to ethernet.");
+                    break;
+                case connectivity.connectionType.bluetooth:
+                    this.connectionType = "Bluetooth";
+                    console.log("Connection type changed to bluetooth.");
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        bluetooth.hasCoarseLocationPermission().then(
+            function (granted) {
+                console.log("Has Location Permission? " + granted);
+                if (granted) {
+                    this.scanBluetooth();
+                } else {
+                    this.requestBluetooth()
+                }
+            }
+        );
+    }
+
+    requestBluetooth() {
+        bluetooth.requestCoarseLocationPermission().then(
+            function (granted) {
+                console.log("Location permission requested, user granted? " + granted);
+            }
+        );
+    }
+
+    scanBluetooth() {
+        bluetooth.startScanning({
+            serviceUUIDs: ["L2CAP"],
+            seconds: 4,
+            onDiscovered: function (peripheral) {
+                console.log("Periperhal found with UUID: " + peripheral.UUID);
+            },
+            skipPermissionCheck: false,
+        }).then(function () {
+            console.log("scanning complete");
+        }, function (err) {
+            console.log("error while scanning: " + err);
+        });
     }
 
     ngOnInit(): void {
         // Init your component properties here.
+    }
+
+    getConnType() {
+        // result is ConnectionType enumeration (none, wifi or mobile)
+        const myConnectionType = getConnectionType();
+
+        switch (myConnectionType) {
+            case connectionType.none:
+                // Denotes no Internet connection.
+                console.log("No connection");
+                break;
+            case connectionType.wifi:
+                // Denotes a WiFi connection.
+                console.log("WiFi connection");
+                break;
+            case connectionType.mobile:
+                // Denotes a mobile connection, i.e. cellular network or WAN.
+                console.log("Mobile connection");
+                break;
+            case connectionType.ethernet:
+                // Denotes a ethernet connection.
+                console.log("Ethernet connection");
+                break;
+            // Bluetooth functionality in master branch (to be released with 5.0.0)
+            case connectionType.bluetooth:
+                // Denotes a bluetooth connection.
+                console.log("Bluetooth connection");
+                break;
+            default:
+                break;
+        }
+    }
+
+    ngOnDestroy(): void {
+        connectivity.stopMonitoring();
+        bluetooth.stopScanning().then(function () {
+            console.log("scanning stopped");
+        });
     }
 }
